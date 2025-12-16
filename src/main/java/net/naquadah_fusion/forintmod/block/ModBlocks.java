cf809f9fad5c0cdb69ab3436ff5e8aab13e431cc
vgeri108 <vagany.gergely@gmail.com>
2025-12-16 15:54:40 +0100

    Pénz blokkok kezdés
    Pénz blokkok textúrája és regisztrálása

---
+++ src/main/java/net/naquadah_fusion/forintmod/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package net.naquadah_fusion.forintmod.block;
+
+import net.naquadah_fusion.forintmod.ForintMod;
+import net.naquadah_fusion.forintmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import java.util.function.Supplier;
+
+public class ModBlocks
+{
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, ForintMod.MOD_ID);
+
+    public static final RegistryObject<Block> Block500Ft = registerBlock("500ft_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+    public static final RegistryObject<Block> Block1000Ft = registerBlock("1000ft_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
